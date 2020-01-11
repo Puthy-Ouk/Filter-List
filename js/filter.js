1209df2aa211dda9d544acdc73b2966bc41a6bfb
puthy.ouk <puthy.ouk@PNC.LAN>
2020-01-11 16:34:41 +0700

    Show only name have in list

--- js/filter.js
+++ js/filter.js
@@ -1,14 +1,21 @@
+
+
 function onFilterInput(){
-    let getValues = document.querySelector("#inputFilterId");
-    let filter = getValues .value.toUpperCase();
-    let liElements = document.getElementsByTagName("li");
-    let textValue;
-    
+    var getValues = document.querySelector("#inputFilterId");
+    var filter = getValues .value.toUpperCase();
+    var liElements = document.getElementsByTagName("li");
+    var texValues;
     for( let i = 0 ; i < liElements.length ;i++){
-        textValue = liElements[i].textContent || liElements[i].innerText;
-        console.log(textValue.indexOf(filter));
-    }
+    texValues = liElements[i].textContent || liElements[i].innerText;
+    console.log(texValues.indexOf(filter ));
+    //use indexof(filter) because if return 0 it is true if return -1 it is false
+    var  inFind = texValues.toUpperCase().indexOf(filter ) > -1;
+    if(inFind ){
+       liElements[i].style.display = 'block';
+    }else{
+        liElements[i].style.display = 'none';
+    }   
+}
 }
 
-document.addEventListener('keyup',onFilterInput)
-
+document.addEventListener('keyup',onFilterInput);
